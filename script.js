56af7dc348d61509e6faf5c3a53f15eb5d796089
tourinhoyasmin <145395964+tourinhoyasmin@users.noreply.github.com>
2025-04-01 14:56:03 -0300

    01.04

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-    consto opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    const botaoDeAcessibilidade =document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade =document.getElementById('opcoes-acessibilidade');
 
-    botaoDeAcessibilidade.addEventListener ('click' , function (){
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao')
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    botaoDeAcessibilidade.addEventListener('click', function (){
+            botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+            opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
     });
+
+
+
+
+
+
+
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
