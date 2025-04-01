05490313df3bc175e7503cf928105a64ab442bf1
tourinhoyasmin <145395964+tourinhoyasmin@users.noreply.github.com>
2025-04-01 14:21:08 -0300

    01.04

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById ('aumentar-fonte')
-    
+    const diminuiFonteBotao = document.getElementById ('diminuir-fonte')
+
     let tamanhoAtualFonte = 1
 
     aumentaFonteBotao.addEventListener('click', {
