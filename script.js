967eda7c5309d7c3c8334632f44040633d4e2e0d
tourinhoyasmin <145395964+tourinhoyasmin@users.noreply.github.com>
2025-04-22 14:01:38 -0300

    22.04

--- script.js
+++ script.js
@@ -7,14 +7,12 @@ document.addEventListener('DOMContentLoaded', function(){
                 botaoDeAcessibilidade.classList.toggle('rotacao-botao');
                 opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
         });
+
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
     
-    
-    
-    
-    
-    
-    
-    
+
+
         const aumentaFonteBotao = document.getElementById('aumentar-fonte');
         const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     
