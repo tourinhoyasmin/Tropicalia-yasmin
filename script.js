fcd05e7f45cca96b1d491ebc8ac87cde0f01060c
tourinhoyasmin <145395964+tourinhoyasmin@users.noreply.github.com>
2025-04-22 14:24:07 -0300

    22.04

--- script.js
+++ script.js
@@ -34,6 +34,9 @@ document.addEventListener('DOMContentLoaded', function(){
             document.body.classList.toggle('alto-contraste');
         });
     
-    
-    
         });
+
+        ScrollReveal().reveal('#inicio', {delay: 500});
+        ScrollReveal().reveal('#galeria', {delay: 500});
+        ScrollReveal().reveal('#tropicalia', {delay: 500});
+        ScrollReveal().reveal('#contato', {delay: 500});
