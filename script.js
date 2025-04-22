966f7211c52e1f3a62b19160d9be62eb2f4a10a7
tourinhoyasmin <145395964+tourinhoyasmin@users.noreply.github.com>
2025-04-22 13:53:43 -0300

    22.04

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function(){
         const aumentaFonteBotao = document.getElementById('aumentar-fonte');
         const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     
-        const alternaContraste = document.getElementById('alterna-contraste')
+        const alternaContraste = document.getElementById('alterna-contraste');
         
         let tamanhoAtualFonte = 1;
         
